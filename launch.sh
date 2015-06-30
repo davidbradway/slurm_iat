6b8bb4ef32d36786e382aaea72dc9e030ed9f440
David Bradway <david.bradway@gmail.com>
2015-06-30 15:39:57 -0400

    fix typo, add help

--- launch.sh
+++ launch.sh
@@ -1,4 +1,3 @@
-#Then launch the batch process using the --array option to specify the indexes.
+#Then launch the batch process using the --array option to specify the indices.
 
 sbatch --array=1-2 array_job.sh
-
