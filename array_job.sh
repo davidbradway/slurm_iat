b6c3bbab0c64941dc0efe25921330a718208a4e4
David Bradway <david.bradway@gmail.com>
2015-08-10 14:16:30 -0400

    make general, remove my path and NetID

--- array_job.sh
+++ array_job.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
 #
 #
-#SBATCH -J dpb6test # A single job name for the array
+#SBATCH -J test # A single job name for the array
 #SBATCH -n 1 # Number of cores
 #SBATCH -N 1 # All cores on one machine
 #SBATCH --mem 4000 # Memory request
 #SBATCH -t 0-2:00 # 2 hours (D-HH:MM)
-#SBATCH -o output/dpb6test%A%a.out # Standard output
-#SBATCH -e output/dpb6test%A%a.err # Standard error
+#SBATCH -o output/test%A%a.out # Standard output
+#SBATCH -e output/test%A%a.err # Standard error
 #SBATCH --mail-type=END # Type of email notification- BEGIN,END,FAIL,ALL
-#SBATCH --mail-user=dpb6@duke.edu # Email to which notifications will be sent
+##SBATCH --mail-user=NETID@duke.edu # Email to which notifications will be sent
 
-cd /getlab/dpb6/repos/slurmtest
+#cd /getlab/NETID/test
 
 matlab -nojvm -nodisplay -r "a=${SLURM_ARRAY_TASK_ID};myScript,save('output/file_${SLURM_ARRAY_TASK_ID}.mat','b');"
